cea24afea54c3153bdd707a446a59f4bd659a46b
Ashique Mahmood <ashiquemahmood@outlook.com>
2021-07-03 18:10:34 +1000

    added react fe

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 
 morgan.token('reqBody',(req,res)=> JSON.stringify(req.body))
 
